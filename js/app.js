5fc31c4f811e50e5e1498022c20a83efd9877ad1
Alexander A. Yazvetsky <ayazvetsky@air.io>
2022-06-04 19:40:33 +0100

    add other operations using switch-case

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 function getFirstOperator(str) {
   for(let i = 0; i < str.length; i += 1) {
-    if (str[i] == "+") {
-      return i;
+    switch(str[i]) {
+      case "+":
+      case "-":
+      case "*":
+      case "/":
+        return i;
+        break;
     }
   }
   return -1;
