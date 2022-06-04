6ea12a21a4ba197e01e225f6575b41dd36808dcd
Alexander A. Yazvetsky <ayazvetsky@air.io>
2022-06-04 18:55:32 +0100

    add -, *, / operations

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 function getFirstOperator(str) {
   for(let i = 0; i < str.length; i += 1) {
-    if (str[i] == "+") {
+    if (str[i] == "+" || str[i] == "-" || str[i] == "/" || str[i] == "*") {
       return i;
     }
   }
